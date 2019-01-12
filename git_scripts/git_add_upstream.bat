386c24fcca57b4ee709fd7de651cabd6cbe7bf90
rusefi <rusefillc@gmail.com>
2019-01-12 11:46:21 -0500

    update

--- git_scripts/git_add_upstream.bat
+++ git_scripts/git_add_upstream.bat
@@ -1,6 +1,6 @@
 echo Showing the status prior to adding upstead
 git remote -v
-echo Trying to add 'upsteam' 
-git remote add upstream https://github.com/rusefi/kicad-libraries.git
+echo Trying to add 'official_rusefi_upsteam' 
+git remote add official_rusefi_upstream https://github.com/rusefi/kicad-libraries.git
 echo Showing the status after adding upstead
 git remote -v
--- git_scripts/git_reset_to_upstream.bat
+++ git_scripts/git_reset_to_upstream.bat
@@ -1,4 +1,4 @@
-git fetch upstream
+git fetch official_rusefi_upstream
 git checkout master
-git reset --hard upstream/master
+git reset --hard official_rusefi_upstream/master
 git push origin master -f
