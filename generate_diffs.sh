51e63e5387ad8a71ebcceff78dd72571a27913c3
David Holdeman <david@chuckwagoncomputing.com>
2020-08-14 20:52:41 -0500

    still need to generate the normal diff

--- generate_diffs.sh
+++ generate_diffs.sh
@@ -11,15 +11,19 @@
 #!/bin/bash
 
 function gendiffs() {
-  OLDHASH=$(git rev-parse --short HEAD~1)
   if [ -f "$(dirname "$1")/version.txt" ] && [ $(git rev-list -1 $(git rev-parse HEAD) "$(dirname "$1")/version.txt") = $(git rev-parse HEAD) ]; then
    SUFFIX="_release"
    OLDHASH=$(git rev-list -2 $(git rev-parse HEAD) hardware/brain_board/brain_board_STM32F407.kicad_pcb | tail -n1 | head -c7)
+    ../KiCad-Diff/kidiff_linux.py -w -s Git -b $OLDHASH -a $(git rev-parse --short HEAD) -d :0 $1
+    if [ -d $(dirname "$1")/plots ] && [ -n "$RUSEFI_FTP_SERVER" ]; then
+      lftp -u "$RUSEFI_DOXYGEN_FTP_USER","$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" -e \
+        "mirror -Re $(dirname "$1")/plots/ /diffs/plots_$(basename "$1" .kicad_pcb)_release; exit"
+    fi
   fi
-  ../KiCad-Diff/kidiff_linux.py -w -s Git -b $OLDHASH -a $(git rev-parse --short HEAD) -d :0 $1
+  ../KiCad-Diff/kidiff_linux.py -w -s Git -b $(git rev-parse --short HEAD~1) -a $(git rev-parse --short HEAD) -d :0 $1
   if [ -d $(dirname "$1")/plots ] && [ -n "$RUSEFI_FTP_SERVER" ]; then
     lftp -u "$RUSEFI_DOXYGEN_FTP_USER","$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" -e \
-      "mirror -Re $(dirname "$1")/plots/ /diffs/plots_$(basename "$1" .kicad_pcb)$SUFFIX; exit"
+      "mirror -Re $(dirname "$1")/plots/ /diffs/plots_$(basename "$1" .kicad_pcb); exit"
   fi
 }
 export -f gendiffs
