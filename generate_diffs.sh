16426738348478d951456512a423c11adb441fb8
David Holdeman <david@chuckwagoncomputing.com>
2020-08-14 20:11:47 -0500

    Add diff script

---
+++ generate_diffs.sh
@@ -0,0 +1,27 @@
+#####################################################################################
+#                                 generate_diffs.sh                                 #
+#                                    08/13/2020                                     #
+#                             Written By David Holdeman                             #
+#   Checks for changed .kicad_pcb files, generates visual diffs, and uploads them   #
+#                                      to FTP.                                      #
+#####################################################################################
+# The FTP location this uploads to is /diffs/plot_<name of kicad_pcb file, without the extension>
+# In that directory are subdirectorys containing generated SVGs, as well as a 'web' subdirectory,
+# which contains index.html. Open that in a web browser and you get a nice visual diff of each layer.
+#!/bin/bash
+
+function gendiffs() {
+  OLDHASH=$(git rev-parse --short HEAD~1)
+  if [ -f "$(dirname "$1")/version.txt" ] && [ $(git rev-list -1 $(git rev-parse HEAD) "$(dirname "$1")/version.txt") = $(git rev-parse HEAD) ]; then
+   SUFFIX="_release"
+   OLDHASH=$(git rev-list -2 $(git rev-parse HEAD) hardware/brain_board/brain_board_STM32F407.kicad_pcb | tail -n1 | head -c7)
+  fi
+  ../KiCad-Diff/kidiff_linux.py -w -s Git -b $OLDHASH -a $(git rev-parse --short HEAD) -d :0 $1
+  if [ -d $(dirname "$1")/plots ] && [ -n "$RUSEFI_FTP_SERVER" ]; then
+    lftp -u "$RUSEFI_DOXYGEN_FTP_USER","$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" -e \
+      "mirror -Re $(dirname "$1")/plots/ /diffs/plots_$(basename "$1" .kicad_pcb)$SUFFIX; exit"
+  fi
+}
+export -f gendiffs
+
+find . -name "*.kicad_pcb" -exec bash -c 'gendiffs "$0"' {} \;
