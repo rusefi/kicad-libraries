5e8b6af089c2ae82baa6ba75480e1c0b8dc82191
David Holdeman <david@chuckwagoncomputing.com>
2021-10-27 16:21:01 -0500

    add paths

--- generate_diffs.sh
+++ generate_diffs.sh
@@ -10,15 +10,19 @@
 # which contains index.html. Open that in a web browser and you get a nice visual diff of each layer.
 #!/bin/bash
 
+export PATH="$PATH:$(readlink -f ../KiCad-Diff/)"
+export LD_LIBRARY_PATH="/usr/lib/kicad-nightly/lib/x86_64-linux-gnu/"
+export PYTHONPATH="/usr/lib/kicad-nightly/lib/python3/dist-packages"
+
 function gendiffs() {
   if [ -f "$(dirname "$1")/version.txt" ] && [ $(git rev-list -1 $(git rev-parse HEAD) "$(dirname "$1")/version.txt") = $(git rev-parse HEAD) ]; then
    OLDHASH=$(git rev-list -2 $(git rev-parse HEAD) "$1" | tail -n1 | head -c7)
-    ../KiCad-Diff/kidiff_linux.py -w -s Git -b $OLDHASH -a $(git rev-parse --short HEAD) -d :0 $1
+    kidiff -w -s Git -b $OLDHASH -a $(git rev-parse --short HEAD) -d :0 $1
     if [ -d $(dirname "$1")/plots ] && [ -n "$RUSEFI_SSH_SERVER" ]; then
       tar -czf - $(dirname "$1")/plots/* | sshpass -p "$RUSEFI_SSH_PASS" ssh -o StrictHostKeyChecking=no "$RUSEFI_SSH_USER"@"$RUSEFI_SSH_SERVER" "tar -xzf - -C diffs/plots_$(basename "$1" .kicad_pcb)_release"
     fi
   fi
-  ../KiCad-Diff/kidiff_linux.py -w -s Git -b $(git rev-parse --short HEAD~1) -a $(git rev-parse --short HEAD) -d :0 $1
+  kidiff -w -s Git -b $(git rev-parse --short HEAD~1) -a $(git rev-parse --short HEAD) -d :0 $1
   if [ -d $(dirname "$1")/plots ] && [ -n "$RUSEFI_SSH_SERVER" ]; then
     tar -czf - $(dirname "$1")/plots/* | sshpass -p "$RUSEFI_SSH_PASS" ssh -o StrictHostKeyChecking=no "$RUSEFI_SSH_USER"@"$RUSEFI_SSH_SERVER" "tar -xzf - -C diffs/plots_$(basename "$1" .kicad_pcb)_release"
   fi
