3c3e5d7506914eaba6874a89064585e92ef3d06e
David Holdeman <david@chuckwagoncomputing.com>
2022-01-28 17:55:57 -0600

    move files to other folder

--- generate_diffs.sh
+++ generate_diffs.sh
@@ -20,12 +20,15 @@ function gendiffs() {
     which kidiff -w -s Git -b $OLDHASH -a $(git rev-parse --short HEAD) -d :0 "$1"
   fi
   kidiff -w -s Git -b $(git rev-parse --short HEAD~1) -a $(git rev-parse --short HEAD) -d :0 "$1"
-  if [ $(wc -l "kidiff/hardware/$(dirname "$1")/diff.txt" | cut -d ' ' -f 1) -eq 0 ]; then
-   rm -r "kidiff/hardware/$(dirname "$1")"
+  if [ -s "kidiff/diff.txt" ]; then
+   mkdir ../kidiff/"$(dirname "$1")"
+   mv kidiff/* ../kidiff/"$(dirname "$1")"
+  else
+   rm -r kidiff
   fi
 }
 export -f gendiffs
 
+mkdir ../kidiff
 find . -name "*.kicad_pcb" -not -path "./rusefi_lib/*" -exec bash -c 'gendiffs "$0"' {} \;
-find kidiff -empty -type d -delete
-tar -czf - -C kidiff . | sshpass -p "$RUSEFI_SSH_PASS" ssh -o StrictHostKeyChecking=no "$RUSEFI_SSH_USER"@"$RUSEFI_SSH_SERVER" "tar -xzf - -C diffs/"
+tar -czf - -C ../kidiff . | sshpass -p "$RUSEFI_SSH_PASS" ssh -o StrictHostKeyChecking=no "$RUSEFI_SSH_USER"@"$RUSEFI_SSH_SERVER" "tar -xzf - -C diffs/"
